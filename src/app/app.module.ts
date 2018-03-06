fe8254fd75c87fb8c5c6b13e7f9ac9e1bfced2a2
Aaron Goodrum <ag505181@gmail.com>
2018-03-05 17:21:23 -0800

    1st init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavCmpComponent } from './navbar/nav-cmp/nav-cmp.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavCmpComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/navbar/nav-cmp/nav-cmp.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavCmpComponent } from './nav-cmp.component';
+
+describe('NavCmpComponent', () => {
+  let component: NavCmpComponent;
+  let fixture: ComponentFixture<NavCmpComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NavCmpComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavCmpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
